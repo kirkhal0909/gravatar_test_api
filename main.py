ef88d088ac214e18845bcbac8e8d264b7dd06254
kirkhal0909 <kirkhal0909@gmail.com>
2021-09-20 17:23:04 +0300

    Download json from gravatar by email as python tool console

---
+++ main.py
@@ -0,0 +1,23 @@
+import requests
+import hashlib
+import sys
+
+def emailMD5(email):
+    hash_object = hashlib.md5(email.encode())
+    return hash_object.hexdigest()
+
+def getGravatarInfo(email):
+    email_hash = emailMD5(email)
+    json_link = "https://ru.gravatar.com/{}.json".format(email_hash)
+    request = requests.get(json_link)
+    result = request.json()['entry'][0]
+
+    return result
+
+if len(sys.argv) < 2:
+    query = input("Enter email: ")
+else:
+    query = sys.argv[1]
+
+result = getGravatarInfo(query)
+print(result)
